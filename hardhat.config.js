e4abef354a27ff3306c91c9e9369a4ed35d11048
JK1331 <jakubkubala3@gmail.com>
2022-09-06 08:09:59 +0200

    init commit

---
+++ hardhat.config.js
@@ -0,0 +1,61 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL 
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {version: "0.5.5"},
+      {version: "0.6.6"},
+      {version: "0.8.8"},
+      {version: "0.8.16"},
+      {version: "0.6.0"},
+    ],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+      blockConfirmations: 6,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    }
+  },   
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
+  namedAccounts: {
+    deployer:{
+      default: 0,
+    },
+    users: {
+      default: 1
+    },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max
+  } 
+};
